309ab6d268c76ceeec20d793c9eba5419ead3b5a
gaspersrsen <gasper.srsen@hotmail.com>
2021-01-02 19:50:52 +0100

    Dodal konstruktor objektov
    vsebuje lastnosti objekta ter njegovo premikanje
    pod vplivom sile (lahko je tudi sila enaka 0)
    vnaprej pa je pripravljena
    tudi kinetična energija

--- Biljard.py
+++ Biljard.py
@@ -8,6 +8,34 @@ pygame.display.set_caption("Projekt-Biljard")
 background_colour = (0,0,0)
 screen.fill(background_colour)
 
+#Konstruktor objektov
+class Objekt:
+    def __init__(self, m, x, y, vx, vy, r):
+        self.m = m #masa
+        self.x = x #pozicija
+        self.y = y
+        self.vx = vx #hitrost
+        self.vy = vy
+        self.r = r #radij
+        self.colour = (0, 0, 0) #barva
+        self.thickness = 1 #debelina
+    
+    def __repr__(self):
+        return "Objekt(m={0.m}, x={0.x}, y={0.y}, vx={0.vx}, vy={0.vy}, r={0.r})".format(self)
+    
+    def kineticnaEnergija(self): #kinetična energija delca
+        return self.m *0.5 * (self.vx * self.vx + self.vy * self.vy)
+
+    def premik(self, dt, ax, ay): #premik delca pod vplivom pospeška za določen čas
+        self.x= self.x + self.vx * dt + 0.5 * ax * dt * dt
+        self.y= self.y + self.vy * dt + 0.5 * ay * dt * dt
+        self.vx= self.vx + ax * dt
+        self.vy= self.vy + ay * dt
+
+
+
+
+#Požene program ter čaka na zaprtje
 pygame.display.flip()
 running = True
 while running:

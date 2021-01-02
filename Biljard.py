1fcd4a6b9ffcc29cf70e3919795bf104d904a1df
gaspersrsen <gasper.srsen@hotmail.com>
2021-01-02 18:58:56 +0100

    Dodal program z osnovnimi nastavitvami za okno

---
+++ Biljard.py
@@ -0,0 +1,17 @@
+import pygame as pygame
+
+
+#Nastavitve okna za program
+(width, height) = (300, 200)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Projekt-Biljard")
+background_colour = (0,0,0)
+screen.fill(background_colour)
+
+pygame.display.flip()
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+pygame.display.set_caption("Projekt-Biljard")

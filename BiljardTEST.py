7275795d5892682e8fdc1f30e1a3e6ab5aa5154e
gaspersrsen <gasper.srsen@hotmail.com>
2021-01-10 10:47:44 +0100

    Dodal gravitacijo, trenje in zračni upor. (upoševa se pri premiku)
    Dodal možnost večjih ali manjših
    časovnih korakov.
    
    Dodal nekaj komentarjev.
    
    Dodal funkcijo, ki zazna ali je
    željena pozicija znotraj
    kakšnega delca.
    
    Dodal zaznavanje pritiska miške znotraj delca.

--- BiljardTEST.py
+++ BiljardTEST.py
@@ -1,6 +1,7 @@
 import pygame
 import math
 import random
+import numpy
 
 
 #Nastavitve okna za program
@@ -11,68 +12,80 @@ pygame.display.set_caption("Projekt-Biljard")
 barvaOzadja = (0,0,0)
 ekran.fill(barvaOzadja)
 
+gravitacija = 0.01
+casovniKorak = 0.5
+kTrenja = 0.005
+kUpor = 0.01
+
+
 #Konstruktor objektov
 class Objekt:
     def __init__(self, m, x, y, vx, vy, radij):
-        self.m = m  #masa
-        self.x = x  #pozicija
+        self.m = m  #Masa
+        self.x = x  #Pozicija
         self.y = y
-        self.vx = vx  #hitrost
+        self.vx = vx  #Hitrost
         self.vy = vy
         self.radij = radij  #radij
-        self.barva = (255, 255, 255)  #barva
-        self.debelina = 2  #debelina
-    
+        self.barva = (255, 255, 255)  #Barva
+        self.debelina = 2  #Debelina
+
     def __repr__(self):
         return "Objekt(m={0.m}, x={0.x}, y={0.y}, vx={0.vx}, vy={0.vy}, r={0.r})".format(self)
     
-    def kineticnaEnergija(self):  #kinetična energija delca
+    def kineticnaEnergija(self):  #Kinetična energija delca
         return self.m *0.5 * (self.vx * self.vx + self.vy * self.vy)
 
-    def premik(self, dt, ax=0, ay=0):  #premik delca pod vplivom pospeška za določen čas
+    def premik(self, dt):  #Premik delca pod vplivom pospeška za določen čas
+        kot = math.atan2(self.vx, self.vy)
+        ax = (-kTrenja - kUpor / self.m / self.m * delec.kineticnaEnergija()) * math.sin(kot) 
+        ay = (-kTrenja - kUpor / self.m / self.m * delec.kineticnaEnergija()) * math.cos(kot) + gravitacija
         self.x= self.x + self.vx * dt + 0.5 * ax * dt * dt
         self.y= self.y + self.vy * dt + 0.5 * ay * dt * dt
         self.vx= self.vx + ax * dt
         self.vy= self.vy + ay * dt
        
-    def prikaz(self):
+    def prikaz(self):  #Prikaz delca na ekranu
         pygame.draw.circle(ekran, self.barva, (self.x, self.y), self.radij, self.debelina)
 
-    def odbojStena(self):
-        if ((self.x >= sirina - self.radij) or (self.x <= self.radij)):
+    def odbojStena(self):  #Interakcija delca s steno
+        if ((self.x >= sirina - self.radij - self.vx*casovniKorak/2) or (self.x <= self.radij - self.vx*casovniKorak/2)):
             self.vx= -self.vx
-        if ((self.y >= visina - self.radij) or (self.y <= self.radij)):
+        if ((self.y >= visina - self.radij - self.vy*casovniKorak/2) or (self.y <= self.radij - self.vy*casovniKorak/2)):
             self.vy= -self.vy
 
 
+def znotrajDelca(Delci, x, y):  #Ali je naša pozicija znotraj kakšnega delca
+    for d in Delci:
+        if math.sqrt((d.x - x) * (d.x - x) + (d.y - y) * (d.y - y)) <= d.radij:
+            return d
+
 """TEST"""
-steviloDelcev=1
+steviloDelcev=3
 mojiDelci=[]
 for i in range(steviloDelcev):
     m = random.randint(1, 10)
-    radij = 15#random.randint(10, 50)
+    radij = 30#random.randint(10, 50)
     x = random.randint(radij, int(sirina - radij))
     y = random.randint(radij, int(visina - radij))
-    vx = 30#random.randint(-30, 30)
-    vy = 30#random.randint(-30, 30)
+    vx = random.randint(-3, 3)
+    vy = random.randint(-3, 3)
     mojiDelci.append(Objekt(m, x, y, vx, vy, radij))
 
-
-for delec in mojiDelci:
-    delec.prikaz()
-
-for i in range(1000):
-    for delec in mojiDelci:
-        delec.odbojStena()
-        delec.premik(1)
-        delec.prikaz()
 """TEST"""
-
 #Požene program ter čaka na zaprtje
-pygame.display.flip()
 running = True
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-pygame.display.set_caption("Projekt-Biljard")
+    ekran.fill(barvaOzadja)
+    for delec in mojiDelci:
+        if delec =! izbranDelec:
+            delec.odbojStena()
+            delec.premik(casovniKorak)
+        delec.prikaz()
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  #Izhod iz simulacije
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:  #Pritisk miške
+            (kazalecX, kazalecY) = pygame.mouse.get_pos()
+            izbranDelec = znotrajDelca(mojiDelci, kazalecX, kazalecY)

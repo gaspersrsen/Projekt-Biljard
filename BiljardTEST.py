060deefbdcb3abff7ab2ff7e1777d1c2ef2bae38
gaspersrsen <gasper.srsen@hotmail.com>
2021-01-11 12:56:56 +0100

    Naredil posebno datoteko za funkcije
    Zbrisal glavno datoteko, da jo
    zamenjam s testno v kateri
    sem vse programiral
    
    Priredil "glavno" datoteko,
    da izvaja kodo pravilno.

--- BiljardTEST.py
+++ BiljardTEST.py
@@ -1,132 +1,15 @@
 import pygame
-import math
-import random
+import Funkcije
 
 
 #Nastavitve okna za program
-sirina = 1600  #px
-visina = 900  #px
-ekran = pygame.display.set_mode((sirina, visina))
+simulacija = Funkcije.Okolje(1600, 900) #Definicija simulacije in dimenzije okna
+ekran = pygame.display.set_mode((simulacija.sirina, simulacija.visina))
 pygame.display.set_caption("Projekt-Biljard")
-barvaOzadja = (0,0,0)
-ekran.fill(barvaOzadja)
 
-"""Spremenljivke simulacije"""
-casovniKorak = 0.1
-gravitacija = 0.99 * casovniKorak
-kTrenja = 0.0001 * casovniKorak
-kUpor = 0.0001 * casovniKorak
+#Delci
+simulacija.dodajDelec(30)
 
-#Konstruktor objektov
-class Objekt:
-    def __init__(self, m, x, y, v, kot, radij):
-        self.m = m  #Masa
-        self.x = x  #Pozicija
-        self.y = y
-        self.v = v  #Hitrost
-        self.kot = kot
-        self.radij = radij  #radij
-        self.barva = (255, 255, 255)  #Barva
-        self.debelina = 2  #Debelina
-
-    def __repr__(self):
-        return "Objekt(m={0.m}, x={0.x}, y={0.y}, v={0.v}, kot={0.kot}, r={0.radij})".format(self)
-
-    def kineticnaEnergija(self):  #Kinetična energija delca
-        return self.m *0.5 * (self.v * self.v)
-
-    def potencialnaEnergija(self): #Potencialna energija delca
-        return self.m * gravitacija * (visina - self.y)
-
-    def pospesek(self, vektor): #Pospešek delca
-        (self.kot, self.v) = sestejVektorja(self.kot, self.v, vektor[0], vektor[1])
-
-    def premik(self, dt):  #Premik delca za nek čas dt
-        (self.kot, self.v) = sestejVektorja(self.kot, self.v, math.pi, gravitacija)
-        (self.kot, self.v) = sestejVektorja(self.kot, self.v, self.kot, -kTrenja - kUpor * self.v * self.v)
-        self.x += self.v * dt * math.sin(self.kot)
-        self.y -= self.v * dt * math.cos(self.kot)  #y je definiran navzdol
-
-    def premikMiska(self, kazalec):
-        razx = kazalec[0] - self.x
-        razy =  kazalec[1] - self.y
-        self.kot = math.pi / 2 + math.atan2(razy, razx)
-        self.v = math.sqrt(razx **2 + razy ** 2) * 0.1
-       
-    def prikaz(self):  #Prikaz delca na ekranu
-        pygame.draw.circle(ekran, self.barva, (self.x, self.y), self.radij, self.debelina)
-
-    def odbojStena(self):  #Interakcija delca s steno
-        if self.x >= sirina - self.radij:
-            self.kot = -self.kot
-            self.x = sirina - self.radij
-        elif self.x <= self.radij:
-            self.kot = -self.kot
-            self.x = self.radij
-        if self.y >= visina - self.radij:
-            self.kot = math.pi - self.kot
-            self.y = visina - self.radij
-        elif self.y <= self.radij:
-            self.kot = math.pi -self.kot
-            self.y = self.radij
-
-def sestejVektorja(kot1, dolzina1, kot2, dolzina2): #Seštevek dveh vekotjev
-    x = math.sin(kot1) * dolzina1 + math.sin(kot2) * dolzina2
-    y = math.cos(kot1) * dolzina1 + math.cos(kot2) * dolzina2
-    kot  = 0.5 * math.pi - math.atan2(y, x) #Pazi ker je y definiran navzdol
-    dolzina = math.sqrt(x ** 2 + y ** 2)
-    return (kot, dolzina)
-
-def razdalja(d1, d2): #Razdalja med dvema delcema
-    razx = d1.x - d2.x
-    razy =  d1.y - d2.y
-    return math.sqrt(razx ** 2 + razy ** 2)
-
-def znotrajDelca(Delci, x, y):  #Ali je naša pozicija znotraj kakšnega delca
-    for d in Delci:
-        if math.sqrt((d.x - x) ** 2 + (d.y - y) ** 2) <= d.radij:
-            return d
-
-
-def odboj(d1, d2):
-    razx = d1.x - d2.x
-    razy =  d1.y - d2.y
-    sestm = d1.m + d2.m
-    razm = d1.m - d2.m
-    r2 = d2.radij + d1.radij
-    odmik = razdalja(d1, d2)
-    kinPred = d1.kineticnaEnergija() + d2.kineticnaEnergija()
-    if (odmik < r2):
-        kotOdboja = math.atan2(razy, razx) + math.pi / 2
-        (d1.kot, d1.v) = sestejVektorja(d1.kot, d1.v * razm / sestm, kotOdboja, 2 * d2.v * d2.m / sestm)
-        (d2.kot, d2.v) = sestejVektorja(d2.kot, d2.v * (-razm) / sestm, kotOdboja + math.pi, 2 * d1.v * d1.m / sestm)
-        kinPo = d1.kineticnaEnergija() + d2.kineticnaEnergija()
-        popravekKin = 1
-        if (kinPo != 0):
-            popravekKin = math.sqrt(kinPred / kinPo)
-            
-        d1.v *= popravekKin
-        d2.v *= popravekKin 
-
-
-        prekrivanje = 0.6 * (r2 - razdalja(d1, d2) + 1)
-        d1.x += math.sin(kotOdboja) * prekrivanje
-        d1.y -= math.cos(kotOdboja) * prekrivanje
-        d2.x -= math.sin(kotOdboja) * prekrivanje
-        d2.y += math.cos(kotOdboja) * prekrivanje
-
-"""TEST"""
-steviloDelcev=100
-mojiDelci=[]
-for i in range(steviloDelcev):
-    m = random.randint(1, 10)
-    radij = random.randint(10, 50)
-    x = random.randint(radij, int(sirina - radij))
-    y = random.randint(radij, int(visina - radij))
-    v = random.randint(-3, 3)
-    kot = random.uniform(-math.pi, math.pi)
-    mojiDelci.append(Objekt(m, x, y, v, kot, radij))
-"""TEST"""
 #Požene program ter čaka na zaprtje
 running = True
 izbranDelec = None
@@ -135,23 +18,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  #Izhod iz simulacije
             running = False
-            print(errors)
         if event.type == pygame.MOUSEBUTTONDOWN:  #Pritisk miške
-            (kazalecX, kazalecY) = pygame.mouse.get_pos()
-            izbranDelec = znotrajDelca(mojiDelci, kazalecX, kazalecY)
+            izbranDelec = simulacija.znotrajDelca(pygame.mouse.get_pos())
         elif event.type == pygame.MOUSEBUTTONUP:
             izbranDelec = None
 
-    ekran.fill(barvaOzadja)  #Simulacija
-    En = 0
+    ekran.fill(simulacija.barva)  #Počistimo ekran
     if izbranDelec:  #Ročni premik delca
         izbranDelec.premikMiska(pygame.mouse.get_pos())
-    for i, delec in enumerate(mojiDelci):
-        delec.premik(casovniKorak)
-        delec.odbojStena()
-        for delec2 in mojiDelci[i+1:]:
-            odboj(delec, delec2)
-        delec.prikaz()
-        En += delec.kineticnaEnergija() + delec.potencialnaEnergija()
-    print(En)
+    
+    simulacija.simuliraj()
+    for delec in simulacija.delci:
+        pygame.draw.circle(ekran, delec.barva, (delec.x, delec.y), delec.radij, delec.debelina)
+    
     pygame.display.flip()
---
+++ Funkcije.py
@@ -0,0 +1,143 @@
+import math
+import random
+
+def sestejVektorja(kot1, dolzina1, kot2, dolzina2): 
+    """Seštevek dveh vekotjev
+    """
+    x = math.sin(kot1) * dolzina1 + math.sin(kot2) * dolzina2
+    y = math.cos(kot1) * dolzina1 + math.cos(kot2) * dolzina2
+    kot  = 0.5 * math.pi - math.atan2(y, x) #Pazi ker je y definiran navzdol
+    dolzina = math.sqrt(x ** 2 + y ** 2)
+    return (kot, dolzina)
+
+def razdalja(d1, d2): 
+    """Razdalja med dvema delcema
+    """
+    razx = d1.x - d2.x
+    razy =  d1.y - d2.y
+    return math.sqrt(razx ** 2 + razy ** 2)
+
+
+
+
+def odboj(d1, d2):
+    """Odboj dveh delcev
+    """
+    razx = d1.x - d2.x
+    razy =  d1.y - d2.y
+    sestm = d1.m + d2.m
+    razm = d1.m - d2.m
+    r2 = d2.radij + d1.radij
+    odmik = razdalja(d1, d2)
+    kinPred = d1.kineticnaEnergija() + d2.kineticnaEnergija()
+    if (odmik < r2):
+        kotOdboja = math.atan2(razy, razx) + math.pi / 2
+        (d1.kot, d1.v) = sestejVektorja(d1.kot, d1.v * razm / sestm, kotOdboja, 2 * d2.v * d2.m / sestm)
+        (d2.kot, d2.v) = sestejVektorja(d2.kot, d2.v * (-razm) / sestm, kotOdboja + math.pi, 2 * d1.v * d1.m / sestm)
+        kinPo = d1.kineticnaEnergija() + d2.kineticnaEnergija()
+        """popravekKin = 1
+        if (kinPo != 0):
+            popravekKin = math.sqrt(kinPred / kinPo)   
+        d1.v *= popravekKin
+        d2.v *= popravekKin"""
+
+        prekrivanje = 0.6 * (r2 - razdalja(d1, d2) + 1)
+        d1.x += math.sin(kotOdboja) * prekrivanje
+        d1.y -= math.cos(kotOdboja) * prekrivanje
+        d2.x -= math.sin(kotOdboja) * prekrivanje
+        d2.y += math.cos(kotOdboja) * prekrivanje
+
+#Konstruktor objektov
+class Objekt:
+    def __init__(self, m, x, y, v, kot, radij, barva):
+        self.m = m  #Masa
+        self.x = x  #Pozicija
+        self.y = y
+        self.v = v  #Hitrost
+        self.kot = kot  #Kot hitrosti
+        self.radij = radij  #Radij
+        self.barva = barva  #Barva
+        self.debelina = 2  #Debelina
+
+    def __repr__(self):
+        return "Objekt(m={0.m}, x={0.x}, y={0.y}, v={0.v}, kot={0.kot}, r={0.radij})".format(self)
+
+    def kineticnaEnergija(self):  #Kinetična energija delca
+        return self.m *0.5 * (self.v * self.v)
+
+    def potencialnaEnergija(self, other): #Potencialna energija delca
+        return self.m * other.gravitacija * (other.visina - self.y)
+
+    def pospesek(self, vektor): #Pospešek delca
+        (self.kot, self.v) = sestejVektorja(self.kot, self.v, vektor[0], vektor[1])
+
+    def premik(self, other):  #Premik delca za nek čas dt
+        (self.kot, self.v) = sestejVektorja(self.kot, self.v, math.pi, other.gravitacija)
+        (self.kot, self.v) = sestejVektorja(self.kot, self.v, self.kot, -other.kTrenja - other.kUpor * self.v * self.v)
+        self.x += self.v * other.casovniKorak * math.sin(self.kot)
+        self.y -= self.v * other.casovniKorak * math.cos(self.kot)  #y je definiran navzdol
+
+    def premikMiska(self, kazalec):
+        razx = kazalec[0] - self.x
+        razy =  kazalec[1] - self.y
+        self.kot = math.pi / 2 + math.atan2(razy, razx)
+        self.v = math.sqrt(razx **2 + razy ** 2) * 0.1
+       
+
+
+
+class Okolje: #Okolje simulacije
+    def __init__(self, sirina, visina, **kargs):
+        self.sirina = sirina
+        self.visina = visina
+        self.delci = []
+        self.barva = kargs.get("barva", (0,0,0))
+        self.casovniKorak = kargs.get("casovniKorak", 0.1)
+        self.gravitacija = kargs.get("gravitacija", 0.0) * self.casovniKorak#0.99 * self.casovniKorak 
+        self.kTrenja = kargs.get("kTrenja", 0.0) * self.casovniKorak#0.0001 * self.casovniKorak
+        self.kUpor = kargs.get("kUpor", 0.0) * self.casovniKorak#0.0001 * self.casovniKorak
+
+    def dodajDelec(self, n = 1, **kargs): 
+        """Dodajanje delcev z možnimi atributi:(m, x, y, v, kot, radij, barva)
+        """
+        for i in range(n):
+            m = kargs.get("m", random.randint(1, 10))
+            radij = kargs.get("radij", random.randint(10, 50))
+            x = kargs.get("x", random.randint(radij, int(self.sirina - radij)))
+            y = kargs.get("y", random.randint(radij, int(self.visina - radij)))
+            v = kargs.get("v", random.randint(-3, 3))
+            kot = kargs.get("kot", random.uniform(-math.pi, math.pi))
+            barva = kargs.get("barva", (255, 255, 255))
+            self.delci.append(Objekt(m, x, y, v, kot, radij, barva))
+    
+    def odbojStena(self, other):  
+        """Interakcija delca s steno
+        """
+        if other.x >= self.sirina - other.radij:
+            other.kot = -other.kot
+            other.x = self.sirina - other.radij
+        elif other.x <= other.radij:
+            other.kot = -other.kot
+            other.x = other.radij
+        if other.y >= self.visina - other.radij:
+            other.kot = math.pi - other.kot
+            other.y = self.visina - other.radij
+        elif other.y <= other.radij:
+            other.kot = math.pi -other.kot
+            other.y = other.radij
+
+    def simuliraj(self): 
+        """Pokliče vse potrebne funkcije, da se delci
+        premaknejo v skladu s simulacijo
+        """
+        for i, delec in enumerate(self.delci):
+            delec.premik(self)
+            self.odbojStena(delec)
+            for delec2 in self.delci[i+1:]:
+                odboj(delec, delec2)
+
+    def znotrajDelca(self, pozicija):  #Ali je naša pozicija znotraj kakšnega delca
+        for d in self.delci:
+            if math.sqrt((d.x - pozicija[0]) ** 2 + (d.y - pozicija[1]) ** 2) <= d.radij:
+                return d
+        
